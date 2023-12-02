981c1ec7af509b30aa3c5b67776f785bbb32459c
Everett Summer <55684557+EverettSummer@users.noreply.github.com>
2023-12-02 00:35:22 -0800

    add sync episodes button for sync episodes with bgm.tv

--- src/app/alert-dialog/alert-dialog.component.ts
+++ src/app/alert-dialog/alert-dialog.component.ts
@@ -3,7 +3,16 @@ import { UIDialogRef } from '@irohalab/deneb-ui';
 
 @Component({
     selector: 'alert-dialog',
-    templateUrl: './alert-dialog.html'
+    templateUrl: './alert-dialog.html',
+    styles: [`
+        :host {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
+        }
+    `]
 })
 export class AlertDialog {
     @Input()
